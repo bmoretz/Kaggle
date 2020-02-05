b0c10ea0cc799ff83ce9a0928716b07f655ea0e2
Brandon Moretz <bmoretz@ionicsolutions.net>
2020-02-05 12:11:20 -0500

    h2o auto ml & rf
    h2o auto ml & rf

--- titanic/h2o.R
+++ titanic/h2o.R
@@ -1,12 +1,8 @@
+library(tidyverse)
 library(data.table)
 library(h2o)
-
-library(ggplot2)
-
-# Utility
+library(recipes)
 library(here)
-
-# Metrics
 library(pROC)
 
 # Global & Theme
@@ -32,91 +28,59 @@ data.dir <- file.path(here::here(), project, "datasets"); submission.dir <- file
 train <- data.table::fread(file.path(data.dir, "train.csv"), 
                            stringsAsFactors = T)
 
-train$Survived <- factor(train$Survived, levels = c(0, 1), labels = c(0, 1))
+train$Survived <- as.factor(train$Survived)
+train[, PassengerId := NULL]
 
 test <- data.table::fread(file.path(data.dir, "test.csv"),
                           stringsAsFactors = T)
 
 # Init h2o
 
-h2o.init(nthreads=-1, max_mem_size = "16gb")
+h2o.init(nthreads=-1, max_mem_size = "32gb")
 
-train.h2o <- as.h2o(train)
-test.h2o <- as.h2o(test)
+# Recipe
 
-# Train / Valid / Test
+blueprint <- recipe(Survived ~ ., data = train) %>%
+  step_other(all_nominal(), threshold = 0.005) %>%
+  step_dummy(Ticket)
 
-splits <- h2o.splitFrame(train.h2o, 
-                         c(.7, .2),
-                         seed = 1234)
+train_h2o <- prep(blueprint, training = train, retain = T) %>%
+  juice() %>%
+  as.h2o()
 
-frame.train <- h2o.assign(splits[[1]], "train.survive")
-frame.valid <- h2o.assign(splits[[2]], "valid.survive")
-frame.test <- h2o.assign(splits[[3]], "test.survive")
+test_h2o <- prep(blueprint, training = train) %>%
+  bake(new_data = test) %>%
+  as.h2o()
 
-rm(splits)
-
-# 
-excluded <- c("PassengerId", "Name", "SibSp")
+# Features
+excluded <- c("Name")
 
 response <- "Survived"
-predictors <- setdiff(setdiff(colnames(train), response), excluded)
+predictors <- setdiff(setdiff(colnames(train_h2o), response), excluded)
 
-n.predictors <- length(predictors)
+n_predictors <- length(predictors)
 
-# Generalized Linear Model
+#=================================
+#  Utilities
+#=================================
 
-h2o.glm <- h2o.glm(
-  x = predictors,
-  y = response,
-  training_frame = train.h2o,
-  family = "binomial",
-  lambda_search = T,
-  alpha = .5,
-  nfolds = 10,
-  seed = 123
-)
-
-# GLM, Diag
-
-summary(h2o.glm)
-
-h2o.glm@model$training_metrics@metrics$AUC
-
-h2o.varimp_plot(h2o.glm)
-
-# glm submission
-
-glm.pred <- h2o.predict(h2o.glm, test.h2o, type = "response")
-
-h2o.glm.results <- data.table(PassengerId = test$PassengerId,
-                              Survived = as.vector(glm.pred$predict))
-
-data.table::fwrite(h2o.glm.results, file = file.path(submission.dir, "h2o.glm.submission.csv"))
-
-# Random Forest  
-
-h2o.rf <- h2o.randomForest(
-  x = predictors,
-  y = response,
-  training_frame = frame.train,
-  validation_frame = frame.valid,
-  ntrees = n.predictors * 20,
-  nfolds = 10,
-  stopping_rounds = 5,
-  stopping_tolerance = 0.001,
-  stopping_metric = "AUC",
-  balance_classes = F,
-  score_each_iteration = T,
-  fold_assignment = "Stratified",
-  seed = 123
-)
-
-# RF, Diag
-
-summary(h2o.rf)
-
-h2o.varimp_plot(h2o.rf)
+export_results <- function(type, h2o_model) {
+  #' Generated a submission file from a h2o model.
+  #'
+  #' Generates predictions from model &
+  #'
+  #' @param data.sheet the information data sheet (excel file)  
+  model_pred <- h2o.predict(h2o_model, test_h2o, type = "prob") %>% as.data.table()
+  
+  results <- data.table(PassengerId = test$PassengerId,
+                        Survived = model_pred %>% pull(predict))
+  
+  submission_path <- file.path(submission.dir, paste0("h2o.", type ,".submission.csv"))
+  
+  data.table::fwrite(results, submission_path)
+  
+  print(paste0("Created: ", submission_path))
+}
 
 results_cross_validation <- function(h2o_model) {
   h2o_model@model$cross_validation_metrics_summary %>% 
@@ -135,8 +99,6 @@ results_cross_validation <- function(h2o_model) {
     return()
 }
 
-results_cross_validation(h2o.rf) -> rf_default
-
 plot_results <- function(df_results) {
   df_results %>% 
     gather(Metrics, Values) %>% 
@@ -148,85 +110,107 @@ plot_results <- function(df_results) {
     labs(title = "Model Performance by Some Criteria Selected", y = NULL)
 }
 
-plot_results(rf_default) +
-  labs(subtitle = "Model: Random Forest (h2o package)")
+#=================================
+#  Generalized Linear Model
+#=================================
 
-# Model performance based on test data: 
-pred_class <- h2o.predict(h2o.rf, frame.test) %>% as.data.frame() %>% pull(predict)
-confusionMatrix(pred_class, factor(as.vector(frame.test$Survived), levels = c(0, 1), labels = c(0, 1)))
+h2o_glm <- h2o.glm(
+  x = predictors,
+  y = response,
+  training_frame = train_h2o,
+  family = "binomial",
+  lambda_search = T,
+  alpha = .5,
+  nfolds = 10,
+  seed = 123
+)
 
-# AUC
+# GLM, Diag
 
-as.numeric.factor <- function(x) { as.numeric(levels(x))[x] }
+summary(h2o_glm)
 
-auc_for_test <- function(model_selected) {
-  actual <- frame.test$Survived %>% as.data.table() %>% pull(Survived) %>% as.numeric.factor()
-  pred_prob <- h2o.predict(model_selected, frame.test) %>% as.data.table() %>% pull(predict) %>% as.numeric.factor()
+h2o_glm@model$training_metrics@metrics$AUC
+
+h2o.varimp_plot(h2o_glm)
+
+# Submission
+
+export_results("glm", h2o_glm) # glm submission, 0.75119
+
+#=================================
+#  Random Forest
+#=================================
+
+as.numeric.factor <- function(x) as.numeric(levels(x))[x]
+
+test_roc <- function(model_selected) {
+  actual <- test_h2o$Survived %>% as.data.table() %>% pull(Survived) %>% as.numeric.factor()
+  pred_prob <- h2o.predict(model_selected, test_h2o) %>% as.data.table() %>% pull(predict) %>% as.numeric.factor()
+  
   return(roc(actual, pred_prob))
 }
 
-rf.auc <- auc_for_test(h2o.rf)
-rf.auc$auc
-
-# Graph ROC and AUC: 
-
-sen_spec_df <- tibble(TPR = rf.auc$sensitivities, FPR = 1 - rf.auc$specificities)
-
-sen_spec_df %>% 
-  ggplot(aes(x = FPR, ymin = 0, ymax = TPR))+
-  geom_polygon(aes(y = TPR), fill = "red", alpha = 0.3)+
-  geom_path(aes(y = TPR), col = "firebrick", size = 1.2) +
-  geom_abline(intercept = 0, slope = 1, color = "gray37", size = 1, linetype = "dashed") + 
-  theme_bw() +
-  coord_equal() +
-  labs(x = "FPR (1 - Specificity)", 
-       y = "TPR (Sensitivity)", 
-       title = "Model Performance for RF Classifier based on Test Data", 
-       subtitle = paste0("AUC Value: ", rf.auc$auc %>% round(2)))
-
-# Function for calculating CM: 
-
-my_cm_com_rf <- function(thre) {
-  du_bao_prob <- h2o.predict(best_model2, frame.test) %>% as.data.frame() %>% pull(p1)
-  du_bao <- case_when(du_bao_prob >= thre ~ 1, 
-                      du_bao_prob < thre ~ 0) %>% as.factor()
-  
-  cm <- confusionMatrix(du_bao, frame.test %>% as.data.table() %>% pull(Survived))
-  
-  return(cm)
+test_confusion <- function(model_selected) {
+  actual <- test_h2o %>% as.data.table() %>% pull(Survived) %>% as.factor()
+  pred <- h2o.predict(model_selected, newdata = test_h2o) %>% as.data.table() %>% pull(predict) %>% as.factor()
+  confusionMatrix(pred, actual)
 }
 
-# Set a range of threshold for classification: 
-my_threshold <- c(0.10, 0.15, 0.35, 0.5)
-results_list_rf <- lapply(my_threshold, my_cm_com_rf)
-
-# Function for presenting prediction power by class:  
-
-vis_detection_rate_rf <- function(x) {
-  
-  results_list_rf[[x]]$table %>% as.data.frame() -> m
-  rate <- round(100*m$Freq[1] / sum(m$Freq[c(1, 2)]), 2)
-  acc <- round(100*sum(m$Freq[c(1, 4)]) / sum(m$Freq), 2)
-  acc <- paste0(acc, "%")
-  
-  m %>% 
-    ggplot(aes(Reference, Freq, fill = Prediction)) +
-    geom_col(position = "fill") + 
-    scale_fill_manual(values = c("#e41a1c", "#377eb8"), name = "") + 
-    theme(panel.grid.minor.y = element_blank()) + 
-    theme(panel.grid.minor.x = element_blank()) + 
-    scale_y_continuous(labels = scales::percent) + 
-    labs(x = NULL, y = NULL, 
-         title = paste0("Survivors when Threshold = ", my_threshold[x]), 
-         subtitle = paste0("Detecting Rate for Survivors: ", rate, "%", ", ", "Accuracy: ", acc))
+test_performance <- function(model_selected) {
+  return(list(roc = test_roc(model_selected), cm = test_confusion(model_selected)))  
 }
 
-gridExtra::grid.arrange(vis_detection_rate_rf(1), 
-                        vis_detection_rate_rf(2), 
-                        vis_detection_rate_rf(3), 
-                        vis_detection_rate_rf(4))
+plot_performance <- function(perf) {
+  
+  print(perf)
+  
+  sen_spec_df <- tibble(TPR = perf$roc$sensitivities, FPR = 1 - perf$roc$specificities)
+  
+  sen_spec_df %>% 
+    ggplot(aes(x = FPR, ymin = 0, ymax = TPR))+
+    geom_polygon(aes(y = TPR), fill = "red", alpha = 0.3)+
+    geom_path(aes(y = TPR), col = "firebrick", size = 1.2) +
+    geom_abline(intercept = 0, slope = 1, color = "gray37", size = 1, linetype = "dashed") + 
+    theme_bw() +
+    coord_equal() +
+    labs(x = "FPR (1 - Specificity)", 
+         y = "TPR (Sensitivity)", 
+         title = "Model Performance for RF Classifier based on Test Data", 
+         subtitle = paste0("AUC Value: ", rf.auc$auc %>% round(2)))
+}
 
-# RF, Tune
+#############################################
+# Base Random Forest
+#############################################
+
+rf_base <- h2o.randomForest(
+  x = predictors,
+  y = response,
+  training_frame = train_h2o,
+  ntrees = n_predictors * 20,
+  nfolds = 10,
+  stopping_rounds = 5,
+  stopping_tolerance = 0.001,
+  stopping_metric = "logloss",
+  balance_classes = F,
+  score_each_iteration = T,
+  seed = 123
+)
+
+# RF, Diag
+
+summary(rf_base)
+
+h2o.varimp_plot(rf_base) # vip
+
+# get CV results
+results_cross_validation(rf_base) %>%
+  plot_results() +
+  labs(subtitle = "Model: Random Forest (Base)")
+
+# performance on test data
+test_performance(rf_base) %>%
+  plot_performance()
 
 #=================================
 #  Full Cartesian Grid Search
@@ -236,12 +220,12 @@ gridExtra::grid.arrange(vis_detection_rate_rf(1),
 
 hyper_grid.h2o <- list(ntrees = seq(50, 500, by = 50),
                        mtries = seq(3, 5, by = 1),
-                       # max_depth = seq(10, 30, by = 10),
-                       # min_rows = seq(1, 3, by = 1),
-                       # nbins = seq(20, 30, by = 10),
+                       #max_depth = seq(10, 30, by = 10),
+                       #min_rows = seq(1, 3, by = 1),
+                       nbins = seq(20, 30, by = 10),
                        sample_rate = c(0.55, 0.632, 0.75))
 
-# The number of models is 90: 
+# The number of models is:
 sapply(hyper_grid.h2o, length) %>% prod()
 
 # Train 6000 Random Forest Models: 
@@ -252,27 +236,30 @@ system.time(grid_cartesian <- h2o.grid(algorithm = "randomForest",
                                        seed = 123, 
                                        nfolds = 10, 
                                        training_frame = frame.train,
-                                       stopping_metric = "AUC", 
+                                       stopping_metric = "logloss", 
                                        hyper_params = hyper_grid.h2o,
                                        search_criteria = list(strategy = "Cartesian")))
 
-# Collect the results and sort by our model performance metric of choice: 
+# Collect the results and sort by our model performance metric of choice:
 grid_perf <- h2o.getGrid(grid_id = "rf_grid1", 
-                         sort_by = "auc", 
+                         sort_by = "logloss", 
                          decreasing = FALSE)
 
 # Best model chosen by validation error: 
-best_model <- h2o.getModel(grid_perf@model_ids[[1]])
+rf_cartesian_best <- h2o.getModel(grid_perf@model_ids[[1]])
 
-# Use best model for making predictions: 
-confusionMatrix(h2o.predict(best_model, frame.test) %>% as.data.table() %>% pull(predict), 
-                frame.test %>% as.data.table() %>% pull(Survived) %>% as.factor())
+summary(rf_cartesian_best)
 
-# RF, Diag
+h2o.varimp_plot(rf_cartesian_best) # vip
 
-summary(best_model)
+# get CV results
+results_cross_validation(rf_cartesian_best) %>%
+  plot_results() +
+  labs(subtitle = "Model: Random Forest (Best Cartesian)")
 
-h2o.varimp_plot(best_model)
+# performance on test data
+test_performance(rf_cartesian_best) %>%
+  plot_performance()
 
 #=================================
 #  Random Discrete Grid Search
@@ -280,7 +267,7 @@ h2o.varimp_plot(best_model)
 
 # Set random grid search criteria: 
 search_criteria_2 <- list(strategy = "RandomDiscrete",
-                          stopping_metric = "AUC",
+                          stopping_metric = "logloss",
                           stopping_tolerance = 0.005,
                           stopping_rounds = 10,
                           max_runtime_secs = 30*60)
@@ -298,59 +285,82 @@ system.time(random_grid <- h2o.grid(algorithm = "randomForest",
 
 # Collect the results and sort by our models: 
 grid_perf2 <- h2o.getGrid(grid_id = "rf_grid2", 
-                          sort_by = "AUC", 
+                          sort_by = "logloss", 
                           decreasing = FALSE)
 
-# Best RF: 
-best_model2 <- h2o.getModel(grid_perf2@model_ids[[1]])
+# Best RF:
+rf_random_best <- h2o.getModel(grid_perf2@model_ids[[1]])
 
-# Use this best model for prediction with some thresholds selected: 
+summary(rf_random_best)
 
-my_cm_com_rf_best2 <- function(thre) {
-  du_bao_prob <- h2o.predict(best_model2, frame.test) %>% as.data.frame() %>% pull(p1)
-  du_bao <- case_when(du_bao_prob >= thre ~ 1, 
-                      du_bao_prob < thre ~ 0) %>% as.factor()
-  
-  cm <- confusionMatrix(du_bao, frame.test %>% as.data.table() %>% pull(Survived))
-  
-  return(cm)
-}
+h2o.varimp_plot(rf_random_best) # vip
 
-results_list_rf_best2 <- lapply(my_threshold, my_cm_com_rf_best2)
+# get CV results
+results_cross_validation(rf_random_best) %>%
+  plot_results() +
+  labs(subtitle = "Model: Random Forest (Best Random)")
 
-vis_detection_rate_rf_best2 <- function(x) {
-  
-  results_list_rf_best2[[x]]$table %>% as.data.frame() -> m
-  rate <- round(100*m$Freq[1] / sum(m$Freq[c(1, 2)]), 2)
-  acc <- round(100*sum(m$Freq[c(1, 4)]) / sum(m$Freq), 2)
-  acc <- paste0(acc, "%")
-  
-  m %>% 
-    ggplot(aes(Reference, Freq, fill = Prediction)) +
-    geom_col(position = "fill") + 
-    scale_fill_manual(values = c("#e41a1c", "#377eb8"), name = "") + 
-    theme(panel.grid.minor.y = element_blank()) + 
-    theme(panel.grid.minor.x = element_blank()) + 
-    scale_y_continuous(labels = scales::percent) + 
-    labs(x = NULL, y = NULL, 
-         title = paste0("Detecting Survival when Threshold = ", my_threshold[x]), 
-         subtitle = paste0("Detecting Survival for Default: ", rate, "%", ", ", "Accuracy: ", acc))
-}
+# performance on test data
+test_performance(rf_random_best) %>%
+  plot_performance()
 
-gridExtra::grid.arrange(vis_detection_rate_rf_best2(1), 
-                        vis_detection_rate_rf_best2(2), 
-                        vis_detection_rate_rf_best2(3),
-                        vis_detection_rate_rf_best2(4))
 # RF, submission
 
-rf.pred.std <- h2o.predict(h2o.rf, test.h2o, type = "prob") # std
-rf.pred.cart <- h2o.predict(best_model, test.h2o, type = "prob") # tuned cartesian
-rf.pred.rand <- h2o.predict(best_model2, test.h2o, type = "prob") # tuned random
+rf_pred_std <- h2o.predict(h2o.rf, test.h2o, type = "prob") # std
+rf_pred_cart <- h2o.predict(best_model, test.h2o, type = "prob") # tuned cartesian
+rf_pred_rand <- h2o.predict(best_model2, test.h2o, type = "prob") # tuned random
   
 h2o.rf.results <- data.table(PassengerId = test$PassengerId,
                               Survived = as.vector(round(rf.pred.rand$predict)))
 
 data.table::fwrite(h2o.glm.results, file = file.path(submission.dir, "h2o.rf.submission.csv"))
+
+
+#=================================
+#  Auto ML
+#=================================
+
+# Use AutoML to find a list of candidate models (i.e., leaderboard)
+
+auto_ml <- h2o.automl(
+  x = predictors, 
+  y = response, 
+  training_frame = train_h2o, 
+  nfolds = 5, 
+  max_runtime_secs = 60 * 60 * 8, # Run for 8 hours
+  max_models = 50,
+  keep_cross_validation_predictions = TRUE, 
+  sort_metric = "logloss", 
+  seed = 123,
+  stopping_rounds = 50, 
+  stopping_metric = "logloss",
+  stopping_tolerance = 0
+)
+
+# Assess the leader board; the following truncates the results to show the top 
+# 25 models. You can get the top model with auto_ml@leader
+auto_ml@leaderboard %>% 
+  as.data.frame() %>%
+  dplyr::select(model_id, auc, logloss, aucpr) %>%
+  dplyr::slice(1:25)
+
+h2o_top_auto <- auto_ml@leader
+
+summary(h2o_top_auto)
+
+h2o.varimp_plot(h2o_top_auto) # vip
+
+# get CV results
+results_cross_validation(h2o_top_auto) %>%
+  plot_results() +
+  labs(subtitle = "Model: Auto ML top(1)")
+
+# performance on test data
+test_performance(h2o_top_auto) %>%
+  plot_performance()
+
+# get submission
+export_results("auto", h2o_top_auto)
 
 ### All done, shutdown H2O    
 h2o.shutdown(prompt=FALSE)
